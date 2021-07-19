c7dd97f65a97489d089e5df87e9b9b3f17ba481a
Michal Marcak <marcakster@gmail.com>
2021-07-19 13:17:03 +0200

    next upload

--- database/migrations/2021_07_19_093054_create_orders_table.php
+++ database/migrations/2021_07_19_093054_create_orders_table.php
@@ -15,7 +15,16 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->text('status');
+            $table->integer('weight');
+            $table->integer('quantity');
+            $table->integer('l_elem');
+            $table->integer('q_elem');
+            $table->integer('h_elem');
             $table->timestamps();
+
+            // dodać relację z Material_id
+            // dodać relację z Client_id
         });
     }
 
--- database/migrations/2021_07_19_104531_create_clients_table.php
+++ database/migrations/2021_07_19_104531_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->integer('ean_code');
-            $table->text('description');
-            $table->integer('width');
-            $table->integer('weight');
-            $table->integer('grammage');
-            $table->integer('type_of_cardboard');
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('clients');
     }
 }
