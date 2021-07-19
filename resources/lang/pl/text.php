9cd8eb8b0ba37c5a87c4e3ec5a1e00bcd37f1d56
Michal Marcak <marcakster@gmail.com>
2021-07-19 08:53:23 +0200

    first upload files

---
+++ resources/lang/pl/text.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'login' => 'Zaloguj',
+    'remember_me' => 'Zapamiętaj mnie',
+    'forgot' => 'Zapomniałeś hasło',
+    'reset_password' => 'Resetuj hasło',
+    'desc_forgot' => 'Zapomniałeś hasła? Nie ma problemu. Po prostu podaj nam swój adres e-mail, a wyślemy Ci link do resetowania hasła, który pozwoli Ci wybrać nowe',
+    'register' => 'Rejestracja',
+    'dashboard' => 'Pulpit',
+    'logout' => 'Wyloguj',
+
+];
