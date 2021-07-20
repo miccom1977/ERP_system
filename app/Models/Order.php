ef3ba34242c21e523dec684047794dc50b11ef96
Misiek M <marcakster@gmail.com>
2021-07-21 00:11:46 +0200

    next4

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
+    public $fillable = ['quantity', 'l_elem', 'q_elem', 'h_elem', 'client_id'];
+
+    public function product(){
+        return $this->belongsTo('App\Models\Product');
+    }
 }
+
